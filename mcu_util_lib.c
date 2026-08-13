de9a37bc2bd39620bd5e115452d630f891038553
Chimipupu <chiminyanyan@gmail.com>
2026-08-13 23:55:44 +0900

    ビット操作マクロのテストコード追加

--- mcu_util_lib.c
+++ mcu_util_lib.c
@@ -39,8 +39,8 @@ int main(void)
     // レジスタの8/16/32bit読み出し関数のテスト
     tmp_u8 = REG_READ_BYTE((uint32_t)&test_reg);
     DEBUG_PRINT("Reg 8bit Func Test: Addr=0x%08X, Value=0x%02X", (uint32_t)&test_reg, tmp_u8);
-    tmp_u16 = REG_READ_HALFWORD((uint32_t)&test_reg);
 
+    tmp_u16 = REG_READ_HALFWORD((uint32_t)&test_reg);
     DEBUG_PRINT("Reg 16bit Func Test: Addr=0x%08X, Value=0x%04X", (uint32_t)&test_reg, tmp_u16);
 
     tmp_u32 = REG_READ_WORD((uint32_t)&test_reg);
@@ -56,7 +56,25 @@ int main(void)
     REG_WRITE_WORD((uint32_t)&test_reg, 0xCCCCCCCC);
     DEBUG_PRINT("Reg 32bit Write Func Test: Addr=0x%08X, Value=0x%08X", (uint32_t)&test_reg, test_reg);
     //---------------------------------------------------
+    // レジスタのビット操作マクロのテスト
+    REG_BIT_SET(test_reg, 0);
+    DEBUG_PRINT("Reg Bit Set Macro Test: Addr=0x%08X, Value=0x%08X", (uint32_t)&test_reg, test_reg);
 
+    REG_BIT_CLR(test_reg, 1);
+    DEBUG_PRINT("Reg Bit Clear Macro Test: Addr=0x%08X, Value=0x%08X", (uint32_t)&test_reg, test_reg);
+
+    REG_BIT_TGL(test_reg, 2);
+    DEBUG_PRINT("Reg Bit Toggle Macro Test: Addr=0x%08X, Value=0x%08X", (uint32_t)&test_reg, test_reg);
+
+    if (REG_BIT_CHK(test_reg, 3))
+    {
+        DEBUG_PRINT("Reg Bit Check Macro Test: Addr=0x%08X, Bit 3 is SET", (uint32_t)&test_reg);
+    }
+    else
+    {
+        DEBUG_PRINT("Reg Bit Check Macro Test: Addr=0x%08X, Bit 3 is CLEAR", (uint32_t)&test_reg);
+    }
+    //---------------------------------------------------
     DEBUG_PRINT("----------------------------------------------------");
 
     return 0;
